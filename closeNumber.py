4292cb64aa3b01ba5f5316b59b27704a26b70c7c
Sakshi208 <76934703+Sakshi208@users.noreply.github.com>
2022-05-23 16:18:00 +0530

    close_number
    assgn problems level 1

---
+++ closeNumber.py
@@ -0,0 +1,23 @@
+#lex_auth_0127136008767324161169
+
+#Assignment 2 : practice problem 13 
+
+def close_number(num1,num2,num3):
+    #start writing your code here
+    list=[]
+    count=0
+    list.append(num1-num2)
+    list.append(num1-num3)
+    list.append(num2-num1)
+    list.append(num2-num3)
+    list.append(num3-num1)
+    list.append(num3-num2)
+    print(list)
+    for i in list:
+        if abs(i)==1 or i==0:
+            count=count+1
+    if(count==2):
+        return True
+    return False
+    
+print(close_number(5,4,2))
